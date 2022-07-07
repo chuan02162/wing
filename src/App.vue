90d40171dc0c622545a69ade08c30d2d24d5fa1a
hyc <865872723@qq.com>
2022-07-07 15:50:47 +0800

    component

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+	<Header></Header>
+	<router-view></router-view>
+	<Footer></Footer>
+</template>
+<style>
+	html,
+	body,
+	#app {
+		height: 100%;
+		margin: 0px;
+		padding: 0px;
+	}
+</style>
